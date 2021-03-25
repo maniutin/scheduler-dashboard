bd45bbe429e26c0e71c14a5b14bdc1a5542eafcc
Nick <maniutin@gmail.com>
2021-03-25 13:45:48 -0400

    implement focused view

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -28,25 +28,51 @@ const data = [
 ];
 
 class Dashboard extends Component {
-  state = {
-    loading: false,
+  constructor(props) {
+    super(props);
+
+    this.selectPanel = this.selectPanel.bind(this);
+  }
+
+  selectPanel = (id) => {
+    this.setState({
+      focused: id,
+    });
   };
 
+  state = {
+    loading: false,
+    focused: null,
+  };
+
+  selectPanel(id) {
+    this.setState((previousState) => ({
+      focused: previousState.focused !== null ? null : id,
+    }));
+  }
+
   render() {
-    const dashboardClasses = classnames("dashboard");
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused,
+    });
 
     if (this.state.loading) {
       return <Loading />;
     }
 
-    const panels = data.map((panel) => (
-      <Panel
-        key={panel.id}
-        id={panel.id}
-        label={panel.label}
-        value={panel.value}
-      />
-    ));
+    const panels = data
+      .filter(
+        (panel) =>
+          this.state.focused === null || this.state.focused === panel.id
+      )
+      .map((panel) => (
+        <Panel
+          key={panel.id}
+          label={panel.label}
+          value={panel.value}
+          onSelect={(event) => this.selectPanel(panel.id)}
+        />
+      ));
 
     return <main className={dashboardClasses}>{panels}</main>;
   }
